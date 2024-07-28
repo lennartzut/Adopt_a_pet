f850e13a7591f54706856251fe3a4261f9ac953f
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-07-28 14:55:49 +0200

    Populate page content

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 from flask import Flask
-import pets_dict
+from pets_dict import pets
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return'''
-  <h1>Adopt a Pet!</h1>
-  <p>Browse through the links below to find your new furry friend:</p>
-  <ul>
-    <li><a href="/animals/dogs">Dogs</a></li>
-    <li><a href="/animals/cats">Cats</a></li>
-    <li><a href="/animals/rabbits">Rabbits</a></li>
-  </ul>
-  '''
+    return'''
+    <h1>Adopt a Pet!</h1>
+    <p>Browse through the links below to find your new furry 
+     friend:</p>
+    <ul>
+      <li><a href="/animals/dogs">Dogs</a></li>
+      <li><a href="/animals/cats">Cats</a></li>
+      <li><a href="/animals/rabbits">Rabbits</a></li>
+    </ul>
+    '''
 
 
-@app.route('/animals/{pet_type}')
+@app.route('/animals/<pet_type>')
 def animals(pet_type):
-  html = f'<h1>List of {pet_type}</h1>'
-  return html
+    html = f'<h1>List of {pet_type}</h1>'
+    html += '<ul>'
+    for pet in pets[pet_type]:
+      html += f'<li>{pet["name"]}</li>'
+    html += '</ul>'
+    return html
 
 
 if __name__ == "__main__":
