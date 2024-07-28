6bba4d633f525ef7780a43aa68e1eb307a5e7117
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-07-28 16:04:16 +0200

    Add templates with separate html code. Update code app.py, to render the templates instead

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from pets_dict import pets
 
 app = Flask(__name__)
@@ -6,43 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '''
-    <h1>Adopt a Pet!</h1>
-    <p>Browse through the links below to find your new furry 
-     friend:</p>
-    <ul>
-      <li><a href="/animals/dogs">Dogs</a></li>
-      <li><a href="/animals/cats">Cats</a></li>
-      <li><a href="/animals/rabbits">Rabbits</a></li>
-    </ul>
-    '''
+    return render_template('index.html')
 
 
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
-    html = f'<h1>List of {pet_type}</h1>'
-    html += '<ul>'
-    for idx, pet in enumerate(pets[pet_type]):
-        html += (f'<li><a href="/animals/{pet_type}/{idx}">'
-                 f'{pet["name"]}</a></li>')
-    html += '</ul>'
-    return html
+    return render_template('animals.html', pet_type=pet_type,
+                           pets=enumerate(pets[pet_type]))
 
 
 @app.route('/animals/<pet_type>/<int:pet_id>')
 def pet(pet_type, pet_id):
     pet = pets[pet_type][pet_id]
-    return f'''
-        <h1>{pet['name']}</h1>
-        <img src="{pet['url']}" alt="{pet['name']}" style="width
-        :300px;height:auto;"><br> <p>{pet['description']}</p>
-        <ul>
-          <li><strong>Breed:</strong> {pet['breed']}</li>
-          <li><strong>Age:</strong> {pet['age']} years</li>
-        </ul>
-        <a href="/animals/{pet_type}">Back to
-        {pet_type.capitalize()}</a>
-        '''
+    return render_template('pet.html', pet_type=pet_type, pet=pet)
 
 
 if __name__ == "__main__":
